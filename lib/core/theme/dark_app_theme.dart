e97e7add85b3fc6b2e1079d7e01d229846fb905f
gonzalezMejia <ricarditozombie@gmail.com>
2022-05-01 22:57:16 -0500

    colorscambio

--- lib/core/theme/dark_app_theme.dart
+++ lib/core/theme/dark_app_theme.dart
@@ -3,25 +3,25 @@ import 'package:flutter/material.dart';
 final ThemeData darkAppTheme = ThemeData(
   // Define the default brightness and colors.
   brightness: Brightness.dark,
-  primaryColor: Colors.black54,
-  primaryColorLight: Colors.black12,
-  primaryColorDark: Colors.black38,
+  primaryColor: Colors.blue,
+  primaryColorLight: Colors.blueAccent,
+  primaryColorDark: Colors.blueGrey,
   highlightColor: Colors.white70,
-  indicatorColor: Colors.black,
+  indicatorColor: Colors.blue,
   hoverColor: Colors.white,
-  scaffoldBackgroundColor: Colors.black,
-  colorScheme:
-      const ColorScheme.dark(secondary: Colors.black12 //this is physics color
-          ),
+  scaffoldBackgroundColor: Colors.blue,
+  colorScheme: const ColorScheme.dark(
+      secondary: Colors.blueAccent //this is physics color
+      ),
   iconTheme: const IconThemeData(
     color: Colors.white70,
     size: 37,
   ),
 
-  floatingActionButtonTheme:
-      const FloatingActionButtonThemeData(
-          sizeConstraints:BoxConstraints(maxHeight: 120,maxWidth: 120,minHeight: 50,minWidth: 50),
-          backgroundColor: Colors.white38),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      sizeConstraints: BoxConstraints(
+          maxHeight: 120, maxWidth: 120, minHeight: 50, minWidth: 50),
+      backgroundColor: Colors.white38),
 
   inputDecorationTheme: const InputDecorationTheme(
       fillColor: Colors.white12,
@@ -41,20 +41,25 @@ final ThemeData darkAppTheme = ThemeData(
     headline2: TextStyle(fontSize: 20.0, color: Colors.white),
     headline3: TextStyle(fontSize: 25.0, color: Colors.white),
     subtitle1: TextStyle(fontSize: 16.0, color: Colors.white),
-    bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Colors.white),
+    bodyText1:
+        TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Colors.white),
     headline6: TextStyle(fontSize: 13.0, color: Colors.white), //item title
     subtitle2: TextStyle(fontSize: 18.0, color: Colors.white70),
     headline4: TextStyle(fontSize: 15.0, color: Colors.white),
     headline5: TextStyle(fontSize: 11.0, color: Colors.white),
     button: TextStyle(fontSize: 12.0, color: Colors.white70),
-    bodyText2: TextStyle(fontSize: 10.0, fontFamily: 'Hind', color: Colors.white), //item subtitle
+    bodyText2: TextStyle(
+        fontSize: 10.0,
+        fontFamily: 'Hind',
+        color: Colors.white), //item subtitle
   ),
   primaryTextTheme: const TextTheme(
     bodyText2: TextStyle(fontSize: 30.0, color: Colors.white),
     headline3: TextStyle(fontSize: 20.0, color: Colors.blue),
     headline2: TextStyle(fontSize: 25.0, color: Colors.white),
     subtitle1: TextStyle(fontSize: 16.0, color: Colors.white),
-    headline4: TextStyle(fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.bold),
+    headline4: TextStyle(
+        fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.bold),
     headline5: TextStyle(fontSize: 15.0, color: Colors.white),
     headline6: TextStyle(fontSize: 14.0, color: Colors.white),
   ),
